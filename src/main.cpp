a4e845a9e5784efa44742a03a1e572040264437d
EllieFroggie <ellie.lewis403@proton.me>
2026-02-13 09:02:25 -0700

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,31 @@ int main() {
   std::signal(SIGKILL, Utils::signal_handler);
 
   std::thread worker((VolumeWorker::async_worker));
-  
   AudioManager::get_all_sinks(VolumeWorker::sink, VolumeWorker::sink_lock);
+
   std::cout << "main(): Worker Thread Started.\n";
 
   while (Utils::keep_running) {
-        auto status = Switchboard::run_serial_subsystem();
+    auto status = Switchboard::run_serial_subsystem()
 
-        if (status == Switchboard::SubsystemState::NormalExit) {
-            std::cout << "main(): Subsystem requested normal shutdown." << std::endl;
-            break;
-        }
-        
-        if (status == Switchboard::SubsystemState::RestartNeeded) {
-            std::cerr << "main(): Restarting serial subsystem..." << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
+    if (status == Switchboard::SubsystemState::NormalExit) {
+      std::cout << "main(): Subsystem requested normal shutdown." << std::endl;
+      break;
     }
+    
+    if (status == Switchboard::SubsystemState::RestartNeeded) {
+      std::cerr << "main(): Restarting serial subsystem..." << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+  }
 
-    std::cout << "main(): Joining worker thread..." << std::endl;
+  std::cout << "main(): Joining worker thread..." << std::endl;
      
-    if (worker.joinable()) {
-        worker.join();
-    }
+  if (worker.joinable()) {
+    worker.join();
+  }
 
-    std::cout << "main(): Shutdown complete. Goodbye." << std::endl;
+  std::cout << "main(): Shutdown complete. Goodbye." << std::endl;
 
-    return 0;
+  return 0;
 }
